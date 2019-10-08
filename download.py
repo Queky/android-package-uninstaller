3c52612694c216620271c0cc6ad6091495eace84
Iñaki Sánchez <inakisanchez_92@hotmail.com>
2019-10-08 21:49:59 +0200

    added all changed

--- download.py
+++ download.py
@@ -2,26 +2,38 @@ import requests
 import zipfile
 import sys
 import os
+import main_program
+
+platform = ""
 
 class download_functions:
 
+    def check_internet_con(self):
+        return False
+
     def download_files(self):
-        print('crea carpeta')
-        os.makedirs('./adb')
-        print('carpeta creada')
+        create_directory(self)
         platform = sys.platform
+        #if can_download:
         if platform == "linux" or platform == "linux2":
             link = 'https://dl.google.com/android/repository/platform-tools-latest-linux.zip'
+            main_program.platform_info.set_platform(self, "linux")
         elif platform == "darwin":
             link = 'https://dl.google.com/android/repository/platform-tools-latest-darwin.zip'
+            main_program.platform_info.set_platform(self, "darwin")
         elif platform == "win32":
             link = 'https://dl.google.com/android/repository/platform-tools-latest-windows.zip'
+            main_program.platform_info.set_platform(self, "win32")
+        else:
+            pass
 
         r = requests.get(link)
         with open('./adb/' + platform +'.zip', 'wb') as f:
             f.write(r.content)
         self.file_extract()
 
+    def create_directory(self):
+        os.makedirs('./adb')
 
     def file_extract(self):
         unzip = zipfile.ZipFile('./adb/' + sys.platform + '.zip', 'r')
--- main_program.py
+++ main_program.py
@@ -2,13 +2,22 @@ import os
 import sys
 import subprocess
 
+
+operative_system = ""
+
 class adb_connection:
 
-    def device_info(self):
+    def device_show_info(self):
         path = str(os.getcwd())+ '\\adb\\platform-tools\\adb devices'
         p = subprocess.Popen(path, stdout=subprocess.PIPE)
         text = p.communicate()[0]
         return text.decode('utf-8')
+
+class platform_info:
+
+    def set_platform(self, platform):
+        operative_system = platform
+
 
 class exit:
 
